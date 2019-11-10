088aa24bfd580ba5dcf485f37845a4e890b7433a
kira187 Desktop <missael133@gmail.com>
2019-11-10 16:00:16 -0600

    MainScene functional

---
+++ Assets/Scripts/BotonJugar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonJugar : MonoBehaviour
+{
+    public string nombreEscenaParaCargar = "GameScene";
+    
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene(nombreEscenaParaCargar);
+    }
+}
